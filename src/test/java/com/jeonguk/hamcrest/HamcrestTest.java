def2bee2623de7989c9aa1bd890b656c0e8c3334
jeonguk <kato1883@gmail.com>
2018-05-17 12:11:43 +0900

    Hamcrest Test case add

--- src/test/java/com/jeonguk/hamcrest/HamcrestTest.java
+++ src/test/java/com/jeonguk/hamcrest/HamcrestTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class HamcrestTest {
 
@@ -32,5 +31,33 @@ public class HamcrestTest {
 
 		assertThat(actualString, is(equalTo("equalTo match")));
 		assertThat(actualList, is(equalTo(Lists.newArrayList("equalTo", "match"))));
+	}
+
+	// not(T)and not(Matcher<T>)
+	@Test
+	public void hamcrestNotMatch_test() {
+		String testString = "troy kingdom";
+
+		assertThat(testString, not("german kingdom"));
+		assertThat(testString, is(not(equalTo("german kingdom"))));
+		assertThat(testString, is(not(instanceOf(Integer.class))));
+	}
+
+	// nullValue()and nullValue(Class<T>)
+	@Test
+	public void hamcrestNullValue_test() {
+		Integer nullObject = null;
+
+		assertThat(nullObject, is(nullValue()));
+		assertThat(nullObject, is(nullValue(Integer.class)));
+	}
+
+	// notNullValue()and notNullValue(Class<T>)
+	@Test
+	public void hamcrestNotNullValue_test() {
+		Integer testNumber = 123;
+
+		assertThat(testNumber, is(notNullValue()));
+		assertThat(testNumber, is(notNullValue(Integer.class)));
 	}
 }
