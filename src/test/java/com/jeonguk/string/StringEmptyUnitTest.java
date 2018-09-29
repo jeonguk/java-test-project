1f217381f5dea33820c2278011dffd97981c0418
jeonguk <kato1883@gmail.com>
2018-09-29 21:19:50 +0900

    Test Code Add

---
+++ src/test/java/com/jeonguk/string/StringEmptyUnitTest.java
@@ -0,0 +1,58 @@
+package com.jeonguk.string;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
+import static org.hamcrest.text.IsEmptyString.isEmptyString;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import com.google.common.base.Strings;
+
+@Slf4j
+public class StringEmptyUnitTest {
+
+    private String text = "jeonguk";
+
+    // Junit
+    @Test
+    public void givenAString_whenCheckedForEmptyUsingJunit_shouldAssertSuccessfully() {
+        assertTrue(!text.isEmpty());
+        assertFalse(text.isEmpty());
+        assertNotEquals("", text);
+        assertNotSame("", text);
+    }
+
+    // Hamcrest
+    @Test
+    public void givenAString_whenCheckedForEmptyUsingHamcrest_shouldAssertSuccessfully() {
+        assertThat(text, not(isEmptyString()));
+        assertThat(text, not(isEmptyOrNullString()));
+    }
+
+    // CommonsLang
+    @Test
+    public void givenAString_whenCheckedForEmptyUsingCommonsLang_shouldAssertSuccessfully() {
+        assertTrue(StringUtils.isNotBlank(text));
+    }
+
+    // AssertJ
+    @Test
+    public void givenAString_whenCheckedForEmptyUsingAssertJ_shouldAssertSuccessfully() {
+        Assertions.assertThat(text).isNotEmpty();
+    }
+
+    // Guava
+    @Test
+    public void givenAString_whenCheckedForEmptyUsingGuava_shouldAssertSuccessfully() {
+        assertFalse(Strings.isNullOrEmpty(text));
+    }
+
+}
